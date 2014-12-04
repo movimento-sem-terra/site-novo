3145985947f518dd71aa1955026ac77aeebf5041
Gabriel Caires <=>
2014-12-04 14:21:04 -0300

    correção banner

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,46 +1,40 @@
-$(function initializeCarousel() {
-  var autoScroll;
 
-  // carousel().find('> a').on('click', scroll);
+function initializeCarousel() {
   var carousels =  $('section.jscarousel');
   carousels.each(function(index,item){
-    var $item = $(item)
-    $item.loop = true
-    
+    var $item = $(item);
+    $item.loop = $item.data('loop');
     $item.total_slides = $item.find('ul li').length;
 
     $item.autoScroll = function(){
-      change_slide(this)
+      change_slide(this);
       if($item.loop == true){
-        $item.interval = setTimeout(function(){ $item.autoScroll() },4000)   
+        $item.interval = setTimeout(function(){ $item.autoScroll() },4000);
       }
     }
 
     $item.handleClick = function(event){
-      target = $item
-      $item.loop = false
-      clearTimeout($item.interval)
-      change_slide(target, $(event.currentTarget).data('direction'))
-      event.preventDefault;
-      return false;
+      target = $item;
+      $item.loop = false;
+      clearTimeout($item.interval);
+      change_slide(target, $(event.currentTarget).data('direction'));
+      event.preventDefault();
     }
 
-    $item.find('> a').on('click', $item.handleClick)
-    $item.interval = setTimeout(function(){ $item.autoScroll() },4000)
-    
-  })
-
+    $item.find('> a').on('click', $item.handleClick);
+    if ($item.loop){
+      $item.interval = setTimeout(function(){ $item.autoScroll() },4000);
+    }
+  });
 
   function change_slide(target, direction){
     var index = parseInt(target.attr('data-index'));
-    direction = direction || 1
+    direction = direction || 1;
     var position = index + direction;
     position = position > target.total_slides-1 ? 0 : position;
     position = position < 0 ? target.total_slides-1 : position;
     target.attr('data-index', position);
   }
+}
 
-  
-
-  
-});
+$(initializeCarousel);
