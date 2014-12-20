0302abe067578bb754875537f860f51931aae9f4
RatoX <rodrigovieira18@gmail.com>
2014-12-19 21:50:53 -0300

    Gerando arquivo com tags

---
+++ _plugins/generate_tags_file.rb
@@ -0,0 +1,30 @@
+require 'jekyll/post'
+require 'json'
+
+module Jekyll
+  class TagsFileGenerator < Generator
+    priority :lowest
+
+    def generate(site)
+      tags = Hash.new
+
+      site.posts.each do |post|
+        post.tags.each do |tag|
+          key = tag.values.first
+
+          if tags[key].class == Array
+            tags[key] << post.name
+          else
+            tags[key] = [post.name]
+          end
+        end
+      end
+
+      File.open('tags.json','w') do |f|
+        f.write(tags.to_json)
+      end
+
+      site.static_files << Jekyll::StaticFile.new(site,site.source,'','tags.json')
+    end
+  end
+end
