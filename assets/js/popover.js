08f7cac26ff8648ffe6870bc0aee54bf590b4a7a
Gabriel Caires <gabrielcairesmar@gmail.com>
2015-01-14 20:22:47 -0300

    WIP: Objetivos

--- assets/js/popover.js
+++ assets/js/popover.js
@@ -1,14 +1,33 @@
 function popover() {
-  $('.popover li').each(function (index, element) {
-    element = $(element);
-    element.find('.inner-content').prepend("<span class= 'arrow-pointer'> * </span>");
-    element.find('a').on('click', function (e) {
+  var opened = [];
+  //
+  if ($('.popover').length > 0){
+    //
+    $('.popover li a').each(
+      function (index, element) {
+        element = $(element);
+        element.parent().find('.inner-content').prepend("<span class= 'arrow-pointer'> * </span>");
+        element.on('click', function (e) {
+          $('.popover li').removeClass('open');
+          // TODO adicionar listener para dar um display none ao final da animaçao da div popover
+          $(e.currentTarget).parent().toggleClass('open');
+          opened.push(e.currentTarget);
+          e.preventDefault();
+
+          if (event.stopPropagation) {
+            event.stopPropagation()
+          } else {
+            // For IE
+            event.cancelBubble = true
+          }
+        });
+      }
+    );
+
+    $('body').on('click',function(e){
       $('.popover li').removeClass('open');
-      $(e.currentTarget).parent().addClass('open');
-       e.preventDefault();
     });
   }
-  );
 }
 
-$(popover)
+$(popover);
