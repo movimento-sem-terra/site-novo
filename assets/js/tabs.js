570c93d94b1dc346c1103e911e32146128696789
Gabriel Caires <gabrielcairesmar@gmail.com>
2015-02-05 19:06:36 -0300

    correcao na exibicao do item selecionado

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -25,15 +25,16 @@
       links.each(function(index, link){
         $(link).on('click', function(e){
           var selectedItem = $(e.currentTarget);
+          Tabs.list.find('.selected').removeClass('selected');
           selectedItem.parent().addClass('selected');
           Tabs.changeTab(selectedItem.attr('href'));
+
           e.preventDefault();
         });
       });
     },
 
     changeTab: function(value){
-      this.container.find('.selected').removeClass('selected');
       this.container.find('.tab').removeClass('show');
       this.container.find(value).addClass('show');
     },
