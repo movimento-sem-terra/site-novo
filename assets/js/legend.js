a29f8d912fe7a1bc95a26de1205c2037c92e6606
RatoX <rodrigovieira18@gmail.com>
2015-02-11 11:46:03 -0300

    Ajustando bug rápido

--- assets/js/legend.js
+++ assets/js/legend.js
@@ -2,12 +2,14 @@ var intervalo;
 
 var resizeLegend = function(){
 	var figure = document.querySelector(".image");
-	var widthPattern = figure.children[0].clientWidth;
-	
-	if(widthPattern !== 0){
-		figure.children[1].style.width = widthPattern + "px";
-		clearInterval(intervalo);
-	}
+  if(!!figure && figure.length > 0){
+    var widthPattern = figure.children[0].clientWidth;
+
+    if(widthPattern !== 0 && !!figure.children[1]){
+      figure.children[1].style.width = widthPattern + "px";
+      clearInterval(intervalo);
+    }
+  }
 };
 
 intervalo = setInterval(resizeLegend, 1);
