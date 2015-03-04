4111f9c6cbc767711435db374425657ad23d0765
Mauricio Melo <mauricio@mauriciomelo.com.br>
2015-03-04 18:28:43 -0300

    Adicionando chamada ajax para os periodos

--- assets/js/our-history/timeline.js
+++ assets/js/our-history/timeline.js
@@ -1,12 +1,26 @@
 $(document).ready(function() {
-  $('.entry').click(function() {
-    var pageCenter = $(document).width() / 2;
-    var entryCenter = $(this).offset().left + ($(this).outerWidth() / 2);
-    var timelineLeft = $('.timeline').offset().left;
 
+  var updateTimiline = function(element) {
+    var pageCenter = $(document).width() / 2;
+    var entryCenter = element.offset().left + (element.outerWidth() / 2);
+    var timelineLeft = $('.timeline').offset().left;
+    var period = element.attr("href");
     $('.timeline').offset({left: timelineLeft + (pageCenter - entryCenter) });
 
     $('.entry').removeClass('active');
-    $(this).addClass('active');
-  })
+    element.addClass('active');
+  };
+
+  $('.entry').click(function(e) {
+    e.preventDefault();
+
+    updateTimiline($(this));
+
+    $.get( period + "/content.html" , function( data ) {
+      $("#content").html( data );
+    });
+
+  });
+
+  updateTimiline($('.timeline .entry.active'));
 })
