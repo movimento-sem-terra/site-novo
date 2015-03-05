1272361f6503163ff1cc7fc8bab8e4108cd6adfe
Mauricio Melo <mauricio@mauriciomelo.com.br>
2015-03-05 11:37:34 -0300

    Adicionar funcionalidade de traca de cabeçalho

--- assets/js/our-history/timeline.js
+++ assets/js/our-history/timeline.js
@@ -4,20 +4,28 @@ $(document).ready(function() {
     var pageCenter = $(document).width() / 2;
     var entryCenter = element.offset().left + (element.outerWidth() / 2);
     var timelineLeft = $('.timeline').offset().left;
-    var period = element.attr("href");
+    var headerImgSrc = $('#content .header-img').attr('src');
+
     $('.timeline').offset({left: timelineLeft + (pageCenter - entryCenter) });
 
     $('.entry').removeClass('active');
     element.addClass('active');
+
+
+    $('header .header-img').attr('src', headerImgSrc);
+    $('#background').css('background-image', 'url(' + headerImgSrc + ')');
+    $('#content .header-img').remove();
+
   };
 
   $('.entry').click(function(e) {
     e.preventDefault();
-
-    updateTimiline($(this));
+    var entry = $(this);
+    var period = entry.attr("href");
 
     $.get( period + "/content.html" , function( data ) {
       $("#content").html( data );
+      updateTimiline(entry);
     });
 
   });
