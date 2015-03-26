ceba3496ca456d0337a4b58ba0b6d9a849b33b13
Mauricio Melo <mauricio@mauriciomelo.com.br>
2015-03-26 19:29:39 -0300

    #43 - Adicionando significados da bandeira

--- assets/js/our-symbols/script.js
+++ assets/js/our-symbols/script.js
@@ -11,14 +11,6 @@ $(document).ready(function() {
   $(window).scrollTop(1);
 
 
-  $('#flag img').hover(function() {
-    $('#flag img').css('opacity', .2);
-    $(this).css('opacity', 1);
-  }, function(){
-    $('#flag img').css('opacity', 1);
-  });
-
-
   var distanceFromTop = function(element) {
     return element.offset().top - $(window).scrollTop();
   }
@@ -26,5 +18,25 @@ $(document).ready(function() {
   var distanceFromBottom = function(element) {
     return element.offset().top - $(window).scrollTop() - $(window).height() ;
   }
+
+  var activate = function(target) {
+    $('#symbols-meaning li').removeClass('active');
+    $('li[data-for='+ target +']').addClass('active');
+
+    $('#flag img').removeClass('active').css('opacity', .07);
+    $(target).addClass('active').css('opacity', 1);
+  }
+
+  $('#symbols-meaning li').click(function() {
+    var target = $(this).attr('data-for');
+    activate(target);
+  });
+
+  $('#flag img').click(function() {
+    var target = '#' + $(this).attr('id');
+    activate(target);
+  });
+
+
 
 });
