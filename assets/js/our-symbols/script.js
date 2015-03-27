aa82b0e5944c7d81d23bc83f71dbdc52941c6365
Mauricio Melo <mauricio@mauriciomelo.com.br>
2015-03-27 14:31:30 -0300

    #43 - Adicionando foco inteligente para elementos da baneira para fazer com que ela seja normalizada quando necessario

--- assets/js/our-symbols/script.js
+++ assets/js/our-symbols/script.js
@@ -20,23 +20,59 @@ $(document).ready(function() {
   }
 
   var activate = function(target) {
-    $('#symbols-meaning li').removeClass('active');
-    $('li[data-for='+ target +']').addClass('active');
+    if($(target).is('.active')) {
+      normalizeAll();
+    } else {
+      $('#symbols-meaning li').removeClass('active');
+      $('li[data-for='+ target +']').addClass('active');
 
-    $('#flag img').removeClass('active').css('opacity', .07);
-    $(target).addClass('active').css('opacity', 1);
+      $('#flag img').removeClass('active').css('opacity', .07);
+      $(target).addClass('active').css('opacity', 1);
+    }
   }
 
-  $('#symbols-meaning li').click(function() {
-    var target = $(this).attr('data-for');
+  var normalizeAll = function() {
+    $('#flag img').removeClass('active').css('opacity', 1);
+    $('#symbols-meaning .active').removeClass('active');
+  }
+
+  var hasActive = function() {
+    return $('#flag .active').size() > 0;
+  }
+
+  $('#symbols-meaning li, #flag img').click(function() {
+    var target = $(this).attr('data-for') ? $(this).attr('data-for') : '#' + $(this).attr('id');
     activate(target);
   });
 
-  $('#flag img').click(function() {
-    var target = '#' + $(this).attr('id');
-    activate(target);
+  $('#flag img').not('.active').hover(function() {
+    $('#flag img').not('.active').css('opacity', .07);
+    var opacity = hasActive() ? .3 : 1;
+
+    $(this).not('.active').css('opacity', opacity);
+  }, function() {
+    if(hasActive()) {
+      $('#flag img').not('.active').css('opacity', .07);
+    } else {
+      normalizeAll();
+    }
+
   });
 
+  $('section').not('#our-flag').click(function() {
+    normalizeAll();
+  });
+
+  var timeoutToFadeOut;
+
+  $('#our-flag').mousemove(function() {
+    var meaningsNotActive =   $('#symbols-meaning li').not('.active');
+    clearTimeout(timeoutToFadeOut);
+    $('#symbols-meaning li').fadeIn();
+    timeoutToFadeOut = setTimeout(function() {
+      $('#symbols-meaning li').not('.active').fadeOut(1000);
+    }, 2000);
+  });
 
 
 });
