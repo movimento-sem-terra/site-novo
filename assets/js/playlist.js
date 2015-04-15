8ca74b0325527c63c9baf15ee69b7b9bc3657ee5
Mauricio Melo <mauricio@mauriciomelo.com.br>
2015-04-15 15:21:37 -0300

    #139 - Alterando a classe que indentifica os albuns

--- assets/js/playlist.js
+++ assets/js/playlist.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     if(!player){
       return;
     }
-  
+
     var index = $(link).data('index');
     var src = { ogg: link.data('ogg'),
                 mp3: link.data('mp3')};
@@ -20,19 +20,19 @@ $(document).ready(function() {
 
     var parent = link.parent();
     parent.addClass('active').siblings().removeClass('active');
-    
+
 
     //start player
     $(player).data('current-track', index);
     player.volume = 0.10;
     player.load();
     player.play();
-  
+
   };
 
-  $('.pin').click(function() {
+  $('.album').click(function() {
     var index = $(this).data('index');
-    
+
     player = $('#player_'+index)[0];
 
     player.addEventListener('ended',function(e){
@@ -42,7 +42,7 @@ $(document).ready(function() {
 
       if(next > len){
         next = 0;
-      }      
+      }
 
       console.log(next);
       runMusic($(tracks[next]));
@@ -52,7 +52,7 @@ $(document).ready(function() {
   });
 
   $('.player ol.list li a').click(function(e){
-    e.preventDefault(); 
+    e.preventDefault();
     console.log($(player).data('current-track'));
     runMusic($(this));
     console.log($(player).data('current-track'));
